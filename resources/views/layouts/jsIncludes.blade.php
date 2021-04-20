579dbbe28079795c07cebbcc656cb2e3515e347b
Ibrahim Sharif <shuvoaftab@gmail.com>
2021-04-20 06:10:34 +0600

    Slicing end 1

---
+++ resources/views/layouts/jsIncludes.blade.php
@@ -0,0 +1,12 @@
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="assets/js/jquery-min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/owl.carousel.min.js"></script>
+    <script src="assets/js/wow.js"></script>
+    <script src="assets/js/jquery.nav.js"></script>
+    <script src="assets/js/scrolling-nav.js"></script>
+    <script src="assets/js/jquery.easing.min.js"></script>  
+    <script src="assets/js/main.js"></script>
+    <script src="assets/js/form-validator.min.js"></script>
+    <script src="assets/js/contact-form-script.min.js"></script>
---
+++ resources/views/layouts/toTop.blade.php
@@ -0,0 +1,4 @@
+    <!-- Go to Top Link -->
+    <a href="#" class="back-to-top">
+    	<i class="lni-arrow-up"></i>
+    </a>
